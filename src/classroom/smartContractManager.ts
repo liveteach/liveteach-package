6842b3f60e0fd516f605ac1c26c4793b5c5cad7a
Karim Machlab <karim@vegascity.org>
2023-10-30 13:58:59 +0000

    config references fix

--- src/classroom/smartContractManager.ts
+++ src/classroom/smartContractManager.ts
@@ -7,6 +7,7 @@ import * as historyConfig from "./liveTeachConfigs/historyConfig.json"
 import * as mathConfig from "./liveTeachConfigs/mathConfig.json"
 import * as physicsConfig from "./liveTeachConfigs/physicsConfig.json"
 import { ClassContentFactory } from "./factories/classContentFactory"
+import { ClassroomManager } from "./classroomManager"
 
 export class SmartContractManager {
     private static readonly USE_LOCAL_DATA: boolean = true
@@ -19,14 +20,14 @@ export class SmartContractManager {
     }
 
     static ValidateClassroomGuid(_guid: string): boolean {
-        return classroomConfig.classroom.guid === _guid
+        return ClassroomManager.classroomConfig.classroom.guid === _guid
     }
 
     static async ActivateClassroom(): Promise<string> {
         if (!SmartContractManager.blockchain.userData || !SmartContractManager.blockchain.userData.userId) return ""
 
         if (SmartContractManager.USE_LOCAL_DATA) {
-            switch (classroomConfig.classroom.location) {
+            switch (ClassroomManager.classroomConfig.classroom.location) {
                 case "classroom_1": return "382c74c3-721d-4f34-80e5-57657b6cbc27"
                 default: return ""
             }
@@ -48,7 +49,7 @@ export class SmartContractManager {
 
     static async FetchClassList(): Promise<ClassPacket[]> {
         if (SmartContractManager.USE_LOCAL_DATA) {
-            switch (classroomConfig.classroom.location) {
+            switch (ClassroomManager.classroomConfig.classroom.location) {
                 case "classroom_1": {
                     let classList: ClassPacket[] = []
                     classList.push({
