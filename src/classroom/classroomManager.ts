9e08bad2c329987b89007b4f4855a59dcc97e609
Karim Machlab <karim@vegascity.org>
2023-10-30 13:55:45 +0000

    Passing classroom config to classroom manager

--- src/classroom/classroomManager.ts
+++ src/classroom/classroomManager.ts
@@ -6,7 +6,6 @@ import { Classroom, ClassContent, ClassPacket } from "./classroomObjects";
 import { ClassroomFactory } from "./factories/classroomFactory";
 import { UserDataHelper } from "./userDataHelper";
 import { UserType } from "../enums";
-import * as classroomConfig from "./classroomConfigs/classroomConfig.json"
 import { IClassroomChannel } from "./comms/IClassroomChannel";
 
 export abstract class ClassroomManager {
@@ -14,8 +13,11 @@ export abstract class ClassroomManager {
     static activeClassroom: Classroom = null
     static activeContent: ClassContent = null
     static requestingJoinClass: boolean = false
+    static classroomConfig: any
 
-    static Initialise(_channel: IClassroomChannel): void {
+    static Initialise(_classroomConfig: any, _channel: IClassroomChannel): void {
+        ClassroomManager.classroomConfig = _classroomConfig
+
         SmartContractManager.Initialise()
         CommunicationManager.Initialise(_channel)
     }
@@ -46,7 +48,7 @@ export abstract class ClassroomManager {
         SmartContractManager.FetchClassContent(_id)
         .then(function (classContent) {
             ClassroomManager.activeContent = classContent
-            ClassroomManager.activeClassroom = ClassroomFactory.CreateTeacherClassroom(JSON.stringify(classroomConfig.classroom), ClassroomManager.activeContent.name, ClassroomManager.activeContent.description)
+            ClassroomManager.activeClassroom = ClassroomFactory.CreateTeacherClassroom(JSON.stringify(ClassroomManager.classroomConfig.classroom), ClassroomManager.activeContent.name, ClassroomManager.activeContent.description)
 
             CommunicationManager.EmitClassActivation({
                 id: ClassroomManager.activeClassroom.guid, //use the class guid for students instead of the active content id
