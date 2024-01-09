509f69d563f1013bfe49453a0fa4140f11a9524f
Bobby H <bobbyhiom@googlemail.com>
2024-01-09 09:31:31 +0000

    Removed unused UI

--- src/setup/ui.tsx
+++ src/setup/ui.tsx
@@ -1,10 +1,8 @@
 import { classroomComponent } from "../classroom/ui/ui";
 import { ToastUI } from "../notifications/Toaster";
-import { seatingUIComponent } from "../seating/ui";
 
 export const Render = () => (
   [
-    seatingUIComponent,
     classroomComponent(),
     ToastUI()
     // Add extra UI here
